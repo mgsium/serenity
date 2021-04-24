93903a3a853b21918f264cd3e4086b7a7259ac7a
Musab Guma'a <musabgumaa@gmail.com>
2021-04-24 19:00:15 +0100

    Minimal Landing Page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cx } from "emotion";
 import gs from "../../gs";
+import { Button } from "react-bootstrap";
 
 type Props = {};
 type State = {};
@@ -9,8 +10,13 @@ export default class Home extends React.Component<Props, State> {
     render() {
         return (
             <div className={cx(gs.tabular_center)} style={{ height: "100vh" }}>
-                <div style={{ display: "table-cell", verticalAlign: "middle", fontFamily: "Work Sans, sans-serif" }}>
-                    Serenity
+                <div className="text-center" style={{ display: "table-cell", verticalAlign: "middle", fontFamily: "Work Sans, sans-serif" }}>
+                    <h2 style={{ fontWeight: "bold" }}>Serenity</h2>
+                    <h6 style={{ color: "#666" }}>Notifications without the Stress.</h6>
+                    <br/>
+                    <Button variant="light">
+                        Get Started
+                    </Button>
                 </div>
             </div>
         )
